4d9b0241b34d6801a305fd62f6b00edd9ff38342
Vinicius <vi.ni.cius4@hotmail.com>
2019-07-31 18:14:33 -0300

    Start project

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: 'apinode',
+  ttl: 86400
+}
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const authMiddleware = require('./app/middlewares/auth');
+
+const controllers = require('./app/controllers')
+
+routes.post('/users', controllers.UserController.store);
+routes.post('/sessions', controllers.SessionController.store);
+
+// Down routes needs auth
+routes.use(authMiddleware)
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
+
+
+
+
+module.exports = routes;
